4a1d889e0c64d60fa9f6463e6905af16241ffc51
wari <nicolaycamacho@gmail.com>
2022-09-08 00:49:20 +0200

    Basic components created

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-green-400 px-8 py-2 my-6 text-white'>ADD</button>
+    </div>
+  )
+}
+
+export default Button
