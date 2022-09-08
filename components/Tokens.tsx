16313c95602a73ca6f4e52410680931ab7b03cff
wari <nicolaycamacho@gmail.com>
2022-09-08 13:48:22 +0200

    coingeko api connected to Tokelist component

--- components/Tokens.tsx
+++ components/Tokens.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+// import Image from 'next/image';
 
-const Tokens = () => {
+const Tokens = ({
+  image,
+  name,
+  symbol,
+  price,
+  priceChangeDay,
+  volume,
+  marketcap,
+}: {
+  image: string;
+  name: string;
+  symbol: string;
+  price: number;
+  priceChangeDay: number;
+  volume: number;
+  marketcap: number;
+}) => {
   return (
-    <div>
-      <h1>Tokens</h1>
+    <div className="flex space-x-12 px-12 py-6 mx-12 my-2 justify-between">
+      <img className="w-12" src={image} alt="" />
+      <h1>{name}</h1>
+      <h1>{symbol}</h1>
+      <h1>${price.toLocaleString()}</h1>
+      <h1>{priceChangeDay.toFixed(2)}%</h1>
+      <h1>${volume.toLocaleString()}</h1>
+      <h1>${marketcap.toLocaleString()}</h1>
+
     </div>
-  )
-}
+  );
+};
 
 export default Tokens
