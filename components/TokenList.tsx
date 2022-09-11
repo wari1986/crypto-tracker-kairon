bfa28c9fbb4e5f0bf46563a40f428bebfe820cdf
wari <nicolaycamacho@gmail.com>
2022-09-11 22:53:29 +0200

    price variation percentage for 1h, and 7d added. Token type updated.

--- components/TokenList.tsx
+++ components/TokenList.tsx
@@ -36,7 +36,15 @@ const TokenList = ({ filteredTokens }: { filteredTokens: Array<Token> }) => {
                 name={token.name}
                 symbol={token.symbol}
                 current_price={token.current_price}
-                price_change_24h={token.price_change_percentage_24h}
+                price_change_percentage_1h_in_currency={
+                  token.price_change_percentage_1h_in_currency
+                }
+                price_change_percentage_24h_in_currency={
+                  token.price_change_percentage_24h_in_currency
+                }
+                price_change_percentage_7d_in_currency={
+                  token.price_change_percentage_7d_in_currency
+                }
                 total_volume={token.total_volume}
                 market_cap={token.market_cap}
               />
--- types/Token.ts
+++ types/Token.ts
@@ -20,6 +20,9 @@ type Token = {
   max_supply?: number;
   name: string;
   price_change_24h?: number;
+  price_change_percentage_1h_in_currency: number;
+  price_change_percentage_24h_in_currency: number;
+  price_change_percentage_7d_in_currency: number;
   price_change_percentage_24h?: number;
   roi?: object;
   symbol: string;
