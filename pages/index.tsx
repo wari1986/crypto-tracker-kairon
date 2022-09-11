c76bceab1a7dc545e048776105e86e1ffc1211e1
wari <nicolaycamacho@gmail.com>
2022-09-11 20:59:21 +0200

    Refresh button added, refresh on 30s intervals brake the code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,55 @@
-import Head from 'next/head'
-import InputBar from '../components/InputBar'
-import TokenList from '../components/TokenList';
-import Token from '../types/Token';
-import { useEffect } from 'react';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import TokenList from "../components/TokenList";
+import Token from "../types/Token";
+import { useEffect } from "react";
 
-export default function Home({ filteredTokens }: {filteredTokens: Array<Token>}) {
+export default function Home({
+  filteredTokens,
+}: {
+  filteredTokens: Array<Token>;
+}) {
   // useEffect(() => console.log(filteredTokens));
+  const router = useRouter();
+  //call this function to refresh props
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  const handleRefresh = ()=>{
+    refreshData()
+    console.log('refreshed')
+  }
+  // When setInterval or setTimeout code breaks
+  // setTimeout(() =>{
+  //   handleRefresh()
+  // },20000)
 
   return (
-    <div className='grid place-items-center'>
+    <div className="grid place-items-center">
       <Head>
         <title>Crypto tracker</title>
         <meta name="description" content="Crypto token tracker" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex">
-        {/* <InputBar type='text' placeholder="Add Token ID"/> */}
       </div>
-      <div >
+      <div>
         <TokenList filteredTokens={filteredTokens} />
       </div>
+        <button className='w-1/12 rounded-lg py-2 text-white bg-green-500' onClick={handleRefresh}>Refresh</button>
     </div>
-  )
+  );
 }
 
-export const getServerSideProps = async () => {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-
-  const filteredTokens = await res.json();
-
+  export const getServerSideProps = async (context) => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    );
+    const filteredTokens = await res.json();
   return {
     props: {
-      filteredTokens
-    }
-  }
-}
+      filteredTokens,
+    },
+  };
+};
