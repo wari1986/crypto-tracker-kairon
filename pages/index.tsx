79ba75c9ff479b4d4c6fd7da2e8a513fbd27674e
wari <nicolaycamacho@gmail.com>
2022-09-15 15:51:21 +0200

    react-query installed and use in index fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from 'react-query';
 import Token from "../types/Token";
 import DashboardTable from "../components/Dashboard/DashboardTable";
 import DashboardRefreshButton from "../components/Dashboard/DashboardRefreshButton";
@@ -8,24 +9,40 @@ export default function Home({
 }: {
   filteredTokens: Array<Token>;
 }) {
-  // useEffect(() => console.log(filteredTokens));
 
-  return (
+  const fetchTokens = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
+    );
+    return res.json()
+  }
+
+  const {data, status} = useQuery('filteredTokens', fetchTokens)
+  console.log(data)
+  if (status === 'loading'){
+   return<div>Loading....</div>
+  }
+
+  if (status === 'error'){
+   return<div>Error</div>
+  }
+
+  return(
     <Layout>
-      <DashboardTable filteredTokens={filteredTokens} />
+      <DashboardTable filteredTokens={data} />
       <DashboardRefreshButton />
     </Layout>
   );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
-  );
-  const filteredTokens = await res.json();
-  return {
-    props: {
-      filteredTokens,
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
+//   );
+//   const filteredTokens = await res.json();
+//   return {
+//     props: {
+//       filteredTokens,
+//     },
+//   };
+// };
